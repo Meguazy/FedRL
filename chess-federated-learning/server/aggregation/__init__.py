9bdebc8191483dd3e001bdb5831c12fef18670b9
MeGuaZy <finucci.francesco98@gmail.com>
2025-09-26 12:37:20 +0200

    Create server aggregation package structure and stubs

---
+++ chess-federated-learning/server/aggregation/__init__.py
@@ -0,0 +1,25 @@
+"""
+Server aggregation package.
+
+This package contains aggregation algorithms for federated learning model updates.
+It handles both intra-cluster and inter-cluster aggregation strategies for different
+chess playstyles.
+
+Modules:
+    base_aggregator: Base classes and interfaces for aggregation algorithms
+    intra_cluster_aggregator: Aggregation within clusters (same playstyle)
+    inter_cluster_aggregator: Cross-cluster aggregation for global models
+
+Note: Aggregation modules are currently stubs and need implementation.
+"""
+
+# Import aggregator classes once implemented
+# from .base_aggregator import BaseAggregator
+# from .intra_cluster_aggregator import IntraClusterAggregator
+# from .inter_cluster_aggregator import InterClusterAggregator
+
+__all__ = [
+    # Will be populated when aggregator classes are implemented
+]
+
+__version__ = '1.0.0'
