98ebcdd971f50556c8988ea6f31e4b093316cee1
MeGuaZy <finucci.francesco98@gmail.com>
2025-10-03 15:19:56 +0200

    Expand client package exports and improve package-level documentation

--- chess-federated-learning/client/__init__.py
+++ chess-federated-learning/client/__init__.py
@@ -7,15 +7,49 @@ and model updates.
 
 Modules:
     communication: Client communication protocols and WebSocket client
+    trainer: Training interface and implementations (dummy, supervised, AlphaZero)
+    node: Main federated learning node orchestrator
+    config: Configuration loading and management
 """
 
-# Import communication submodules for convenience
+# Import communication submodules
 from .communication.client_socket import FederatedLearningClient, ClientState, ConnectionStats
 
+# Import trainer components
+from .trainer import (
+    TrainerInterface,
+    TrainingConfig,
+    TrainingResult,
+    TrainingError,
+    DummyTrainer,
+    create_trainer,
+)
+
+# Import node
+from .node import FederatedLearningNode, NodeLifecycleState
+
+# Import config
+from .config import NodeConfig, create_default_config, load_config_with_overrides
+
 __all__ = [
+    # Communication
     'FederatedLearningClient',
     'ClientState',
     'ConnectionStats',
+    # Trainer
+    'TrainerInterface',
+    'TrainingConfig',
+    'TrainingResult',
+    'TrainingError',
+    'DummyTrainer',
+    'create_trainer',
+    # Node
+    'FederatedLearningNode',
+    'NodeLifecycleState',
+    # Config
+    'NodeConfig',
+    'create_default_config',
+    'load_config_with_overrides',
 ]
 
 __version__ = '1.0.0'
